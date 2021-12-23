f608a07397c40765a85c841e7977507d07ac7134
MiruthyanJayanS <77146581+MiruthyanJayanS@users.noreply.github.com>
2021-12-23 11:20:40 +0530

    Create steamlit.py

---
+++ steamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from quickstart import *
+
+with st.form(key='my_form'):
+    name = st.text_input(label='Name')
+    phoneNumber =  st.text_input(label='phoneNumber')
+    emailAddresses = st.text_input(label='emailAddress')
+    submit_button = st.form_submit_button(label='Submit')
+
+
+if submit_button:
+    service=get_service()
+    create_contacts(service,name,phoneNumber,emailAddresses)
